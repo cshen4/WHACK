9e8ad1c4489707141bded4a8870ddf30347c6ba8
Cece Shen <cshen4@wellesley.edu>
2016-10-01 17:24:55 -0400

    additional stuff

---
+++ server/server.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+# app.configselffrom_object(__name__)
+
+import db_model as db
+import json
+
+from datetime import date
+from datetime import timedelta
+
+@app.route("/")
+def index():
+
+    # results = db.select_one()
+    results = db.test()
+    print 'got results'
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run('0.0.0.0')
